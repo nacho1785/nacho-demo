5f18ddd8100e424ab3943a4813934e6123f4d9b8
nacho1785 <jorgeichapperon@gmail.com>
2022-08-18 10:57:59 -0300

    Clock

---
+++ src/components/Clock.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Time extends Component {
+  render (){
+  return (
+    <div className="Time">
+      <h3>Connected</h3>
+    </div>
+  );
+}
+}
+
+export default Time;
