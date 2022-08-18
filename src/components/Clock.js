4c3bfdca95e5d663f4d149b8491e65fa89081c72
nacho1785 <jorgeichapperon@gmail.com>
2022-08-18 12:04:58 -0300

    Update Clock.js

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,10 +1,34 @@
 import React, {Component} from 'react';
+import Clock from 'react-live-clock';
+import { Switch } from 'antd';
+import moment from 'moment';
+import "./Clock.css";
 
 class Time extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            todaysDate: false
+        }
+    }
+
+    toggleSwitch = () => {
+        this.setState({
+            todaysDate: !this.state.todaysDate
+        })
+    }
+
   render (){
   return (
     <div className="Time">
-      <h3>Connected</h3>
+        <div>className="toggleButton"</div>
+            < Switch onClick={this.toggleSwitch} />
+
+      <Clock format={'HH:mm:ss'} ticking={true} timezone={''} />
+    
+    <div className='toggleDate'>
+            {this.state.todaysDate ? moment().format('DD MM YYYY') : ''}
+    </div>
     </div>
   );
 }
